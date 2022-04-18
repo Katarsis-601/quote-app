39eb04b470ce9c94feb1f15f2916fb1717f22174
andriano <andrianow817@gmail.com>
2022-04-18 23:59:09 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
 
 function App() {
+  const [quote, setQuote] = React.useState([]);
+  const [author, seAuthor] = React.useState([]);
+  const [count, setCount] = React.useState();
+  React.useEffect(() => {
+    async function fetchAPI() {
+      const resp = await fetch("https://type.fit/api/quotes").then((data) => {
+        return data.json();
+      });
+      setQuote(resp);
+    }
+    fetchAPI();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul></ul>
+      <button>next</button>
+      <button>previous</button>
     </div>
   );
 }
