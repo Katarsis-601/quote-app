274ded4d7590d3f273d550678d3ee9830c701be0
andriano <andrianow817@gmail.com>
2022-04-19 00:24:48 +0700

    adding navigation

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import React from "react";
 
 function App() {
   const [quote, setQuote] = React.useState([]);
-  const [author, seAuthor] = React.useState([]);
-  const [count, setCount] = React.useState();
+  const [count1, setCount1] = React.useState(0);
+  const [count2, setCount2] = React.useState(1);
   React.useEffect(() => {
     async function fetchAPI() {
       const resp = await fetch("https://type.fit/api/quotes").then((data) => {
@@ -18,9 +18,43 @@ function App() {
 
   return (
     <div className="App">
-      <ul></ul>
-      <button>next</button>
-      <button>previous</button>
+      <ul>
+        {quote.slice(count1, count2).map((data, i) => (
+          <li key={i}>
+            <p>{data.text}</p>
+            <p>{data.author}</p>
+          </li>
+        ))}
+      </ul>
+
+      <button
+        onClick={() => {
+          console.log(count1);
+          if (count1 == 0) {
+            setCount1(quote.length);
+            setCount2(quote.length + 1);
+          } else {
+            setCount1(count1 - 1);
+            setCount2(count2 - 1);
+          }
+        }}
+      >
+        previous
+      </button>
+      <button
+        onClick={() => {
+          console.log(count1);
+          if (count1 == quote.length) {
+            setCount1(0);
+            setCount2(1);
+          } else {
+            setCount1(count1 + 1);
+            setCount2(count2 + 1);
+          }
+        }}
+      >
+        next
+      </button>
     </div>
   );
 }
