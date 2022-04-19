a5c17478a34878ee33f4b48c95314b2b0731d580
andriano <andrianow817@gmail.com>
2022-04-20 02:20:15 +0700

    add styling

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   const [quote, setQuote] = React.useState([]);
   const [count1, setCount1] = React.useState(1);
   const [count2, setCount2] = React.useState(2);
+  const status = React.useRef(null);
   React.useEffect(() => {
     async function fetchAPI() {
       const resp = await fetch("https://type.fit/api/quotes").then((data) => {
@@ -18,44 +19,46 @@ function App() {
 
   return (
     <div className="App">
-      <ul>
-        {quote.slice(count1, count2).map((data, index) => (
-          <li key={index}>
-            <p>{data.text}</p>
-            <p>{data.author}</p>
-          </li>
-        ))}
-      </ul>
+      <div className="content">
+        <ul ref={status}>
+          {quote.slice(count1, count2).map((data, index) => (
+            <li key={index}>
+              <p className="quote">{data.text}</p>
+              <p className="author">{data.author}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
 
-      <button
-        onClick={() => {
-          if (count1 === 1) {
-            setCount1(quote.length - 1);
-            setCount2(quote.length);
-          } else {
-            setCount1(count1 - 1);
-            setCount2(count2 - 1);
-          }
-        }}
-      >
-        previous
-      </button>
-      <button
-        onClick={() => {
-          console.log(count1);
-          if (count1 === quote.length - 1) {
-            setCount1(0);
-            setCount2(1);
-          } else {
-            setCount1(count1 + 1);
-            setCount2(count2 + 1);
-          }
-        }}
-      >
-        next
-      </button>
+      <nav>
+        <button
+          onClick={() => {
+            if (count1 === 1) {
+              setCount1(quote.length - 1);
+              setCount2(quote.length);
+            } else {
+              setCount1(count1 - 1);
+              setCount2(count2 - 1);
+            }
+          }}
+        >
+          previous
+        </button>
+        <button
+          onClick={() => {
+            if (count1 === quote.length - 1) {
+              setCount1(0);
+              setCount2(1);
+            } else {
+              setCount1(count1 + 1);
+              setCount2(count2 + 1);
+            }
+          }}
+        >
+          next
+        </button>
+      </nav>
     </div>
   );
 }
-
 export default App;
